a31ffde1c808c069ed003db7461896729c4f8ea3
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 17:32:47 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,28 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  result = nil
+  array.length.times { |index|
+    if array[index] == value_to_find
+      result = index
+    end
+  }
+  return result
 end
 
 def find_max_value(array)
   # Add your solution here
+   array.length.times { |index|
+    if array[index] == array.max
+      return array.max
+    end
+   }
 end
 
 def find_min_value(array)
   # Add your solution here
+   array.length.times { |index|
+    if array[index] == array.min
+      return array.min
+    end
+   }
 end
